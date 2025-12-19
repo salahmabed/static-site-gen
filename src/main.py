5a72b2512e3bd00f93e95fbfe2b4ea1ac57c9b32
salahmabed <salah.m.abed@gmail.com>
2025-12-19 01:34:58 -0800

    Created HTMLNode.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from textnode import TextType, TextNode
+from textnode import TextNode, TextType
 
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
 
 
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,26 +1,16 @@
 import unittest
 
-from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node1 = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
-        node3 = TextNode("This is a different text node", TextType.BOLD)
-        node4 = TextNode("This is a text node", TextType.ITALIC)
-        node5 = TextNode("This is a different text node still", TextType.LINK, "https://www.achewood.com")
-        self.assertEqual(node1, node2)
-        self.assertNotEqual(node1, node3)
-        self.assertNotEqual(node1, node4)
-        self.assertNotEqual(node1, node5)
-        self.assertNotEqual(node2, node3)
-        self.assertNotEqual(node2, node4)
-        self.assertNotEqual(node2, node5)
-        self.assertNotEqual(node3, node4)
-        self.assertNotEqual(node3, node5)
-        self.assertNotEqual(node4, node5)
-
+        node1 = HTMLNode("a","text inside a paragraph", None, {"href": "https://www.google.com"})
+        node2 = HTMLNode()
+        node3 = HTMLNode("p","more text inside a paragraph")
+        print(node1)
+        print(node2)
+        print(node3)
 
 if __name__ == "__main__":
     unittest.main()
