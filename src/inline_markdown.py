93c22641610e8446cdc4485a96dc493c83c75a02
salahmabed <salah.m.abed@gmail.com>
2025-12-21 15:16:04 -0800

    Extract images & links.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -20,3 +21,27 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+
+def extract_markdown_images(text):
+    link_list = re.findall(r'!\[.*?\]\(.*?\)', text)
+    tuple_list = []
+    for link in link_list:
+        last_alt_index = link.rindex("]")
+        alt_text = link[2:last_alt_index]
+        first_url_index = link.rindex("(")+1
+        url = link[first_url_index:-1]
+        tuple_list.append((alt_text,url))
+    return tuple_list
+
+
+def extract_markdown_links(text):
+    link_list = re.findall(r'(?<!!)\[.*?\]\(.*?\)', text)
+    tuple_list = []
+    for link in link_list:
+        last_alt_index = link.rindex("]")
+        alt_text = link[1:last_alt_index]
+        first_url_index = link.rindex("(")+1
+        url = link[first_url_index:-1]
+        tuple_list.append((alt_text,url))
+    return tuple_list
